b3acdae4c187064973c77579d3f39bab4cc6645b
ysm <tjdals4844@gmail.com>
2020-04-20 16:18:53 +0900

    tuto commit

---
+++ tuto.py
@@ -0,0 +1,12 @@
+
+def arry2D() :
+    a = []
+    for i in range(5) :
+        b = []
+        for j in range(3) :
+            b.append(j) 
+        a.append(b)
+    print(a)
+
+for i in range(10) :
+    arry2D()
